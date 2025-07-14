494151ec0b6d3ed92051e452262b59f2c09849f1
kavitha <kavitha.bonthu@etlsystems.com>
2025-07-14 11:03:34 +0100

    change clock polarity

--- rtl/iddr.v
+++ rtl/iddr.v
@@ -145,11 +145,11 @@ end
     reg [WIDTH-1:0] q_reg_1 = {WIDTH{1'b0}};
     reg [WIDTH-1:0] q_reg_2 = {WIDTH{1'b0}};
 
-    always @(negedge clk) begin
+    always @(posedge clk) begin
         d_reg_1 <= delayed_data_int;
     end
 
-    always @(posedge clk) begin
+    always @(negedge clk) begin
         d_reg_2 <= delayed_data_int;
     end
 
